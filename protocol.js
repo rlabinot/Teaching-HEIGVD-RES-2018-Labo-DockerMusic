3a87f19db1f8716b4bc011ba7c2f88e9f6fdc2de
Labinot Rashiti <labinot.rashiti@heig-vd.ch>
2018-05-05 11:13:47 +0200

    Implementing protocol for musicians part

---
+++ protocol.js
@@ -0,0 +1,12 @@
+// IP configuration
+exports.MULTICAST_IP = "239.255.22.5";
+exports.PORT = 9907;
+exports.AUDITOR_IP = "127.0.0.1";
+
+// Musician parameters
+exports.PIANO = "ti-ta-ti";
+exports.TRUMPER = "pouet";
+exports.FLUTE = "trulu";
+exports.VIOLIN = "gzi-gzi";
+exports.DRUM = "boum-boum";
+exports.MUSICIAN_INTERVAL = 6000;
