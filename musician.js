f890970ece4784d4889ce1a55f7071efda74a551
Labinot Rashiti <labinot.rashiti@heig-vd.ch>
2018-05-07 20:24:35 +0200

    I'm so happy because musician.js works properly !

--- musician.js
+++ musician.js
@@ -8,7 +8,6 @@ const server = dgram.createSocket("udp4");
 // From protocol
 const protocol = require("./protocol");
 
-var instrument = process.argv[2];
 
 var instruments = new Map();
 instruments.set("piano", protocol.PIANO);
@@ -16,16 +15,24 @@ instruments.set("trumpet", protocol.TRUMPER);
 instruments.set("flute", protocol.FLUTE);
 instruments.set("violin", protocol.VIOLIN);
 instruments.set("drum", protocol.DRUM);
-var sound = instruments.get(instrument);
 
-var sendSound = function sendSound() {
-    var payload = JSON.stringify(sound);
-    var message = new Buffer(payload);
-    server.send(message,protocol.PORT,"localhost",(err)=>{
-        server.close();
-    });
-    console.log("Sending payload: " + payload + " via port " + server.address().port);
+
+function Musician(instrument) {
+    this.instrument = instrument;
+
+    Musician.prototype.sendSound = function() {
+        var sound = instruments.get(instrument);
+        var payload = JSON.stringify(sound);
+        var message = new Buffer(payload);
+        server.send(message,protocol.PORT,"localhost",function(err, bytes) {
+			console.log("Sending payload: " + payload + " via port " + server.address().port);
+		});
+    }
+
+    // send the sound every given interval
+    setInterval(this.sendSound.bind(this), protocol.MUSICIAN_INTERVAL);
 }
 
-// send the sound every given interval
-setInterval(sendSound, protocol.MUSICIAN_INTERVAL);
+var instrument = process.argv[2];
+var m1 = new Musician(instrument);
+
